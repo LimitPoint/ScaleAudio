76e6e2a5e3ae036b758b8b2103ed0c71d216adcd
“jpagliaro” <jpagliaro@me.com>
2022-10-18 19:08:45 -0400

    Add links
    Added relevant links to our website `Scaling Audio Files` blog discussion, in file header sections

--- ScaleAudio/Shared/SliderView.swift
+++ ScaleAudio/Shared/SliderView.swift
@@ -2,6 +2,9 @@
 //  SliderView.swift
 //  ScaleAudio
 //
+//  Read discussion at:
+//  http://www.limit-point.com/blog/2022/scale-audio/
+//
 //  Created by Joseph Pagliaro on 2/12/22.
 //  Copyright © 2022 Limit Point LLC. All rights reserved.
 //
